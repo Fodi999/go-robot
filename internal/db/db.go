b506c65558d135a62de63773ab29c195a4a455d9
Fodi999 <fodi85999@gmail.com>
2025-03-02 10:51:33 +0100

    Описание изменений

--- internal/db/db.go
+++ internal/db/db.go
@@ -3,13 +3,18 @@ package db
 import (
 	"database/sql"
 	"log"
-
+	"os"
+    "fmt"
 	_ "github.com/lib/pq" // PostgreSQL драйвер
 )
 
 func Connect() (*sql.DB, error) {
-	// DSN для подключения к базе данных Neon
-	dsn := "postgresql://neondb_owner:npg_6tbnCaKfT5uZ@ep-floral-voice-a9hfvitj-pooler.gwc.azure.neon.tech/neondb?sslmode=require"
+	// Получаем DSN из переменной окружения
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		return nil, fmt.Errorf("переменная окружения DATABASE_URL не установлена")
+	}
+
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, err
